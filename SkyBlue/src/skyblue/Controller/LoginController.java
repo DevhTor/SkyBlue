ced56f0f88aa3131f70d79dae6d486e4c011a4ac
hectoristy <hectoristy@hotmail.com>
2022-03-29 21:10:11 -0400

    comentarios agragados

--- SkyBlue/src/skyblue/Controller/LoginController.java
+++ SkyBlue/src/skyblue/Controller/LoginController.java
@@ -17,45 +17,68 @@ import javax.swing.JOptionPane;
 import skyblue.View.CambiarPassword;
 
 public class LoginController extends MouseAdapter implements ActionListener {
-    Login loginView = new Login();
-    Menu BVV = new Menu();
+    
+    //creacion de objetos
+    Login loginView = new Login();  //vista inicio de sesion
+    Menu BVV = new Menu();  //
     UsuarioDA modeloUsuario = new UsuarioDA();
     CrearCuenta crearCuentaView = new CrearCuenta();
-    public static java.sql.Time time;
     VueloDA VDA = new VueloDA();
     BuscarVuelo BV = new BuscarVuelo();
-     CambiarPassword CP = new CambiarPassword();
+    CambiarPassword CP = new CambiarPassword(); //vista cambiar password
+     
+    public static java.sql.Time time; //para manejar el tiempo
+    
+    //constructor
     public LoginController(Login viewLogin, UsuarioDA modeloUsuario){
-      this.loginView = viewLogin;
-      this.modeloUsuario = modeloUsuario;
-      this.loginView.iniciarSesionBTN.addActionListener(this);
-      this.loginView.crearCuentaBTN.addActionListener(this);
-      this.loginView.claveOlvidadaLBL.addMouseListener(this);
+      
+        
+        this.loginView = viewLogin; 
+        this.modeloUsuario = modeloUsuario;
+        
+        this.loginView.iniciarSesionBTN.addActionListener(this);
+        this.loginView.crearCuentaBTN.addActionListener(this);
+        this.loginView.claveOlvidadaLBL.addMouseListener(this);
     }
      @Override
-      public void mouseClicked(MouseEvent e) {
-         CambiarPasswordController CPC = new CambiarPasswordController(CP, modeloUsuario);
-         CP.setVisible(true);
+      public void mouseClicked(MouseEvent e) { //evento click
+          
+        //instanciando el controlador para cambio de password
+        CambiarPasswordController CPC = new CambiarPasswordController(CP, modeloUsuario);
+        CP.setVisible(true); //mostrando la vista cambio de password
      
      }
      
-    public void actionPerformed(ActionEvent e){
-      if(loginView.iniciarSesionBTN == e.getSource()){
-        String nombre = loginView.nombreUsuarioTXT.getText();
-        String password = new String(loginView.password.getPassword());
-        int iniciado = modeloUsuario.validarUsuario(nombre, password);
-        if(iniciado ==0){
+    public void actionPerformed(ActionEvent e){ 
+        
+      if(loginView.iniciarSesionBTN == e.getSource()){  //si se oprime iniciar sesion
+          
+        String nombre = loginView.nombreUsuarioTXT.getText();   //obtener usuario
+        String password = new String(loginView.password.getPassword()); //obtener password
+        
+        int iniciado = modeloUsuario.validarUsuario(nombre, password);  //validar usuario
+        
+        if(iniciado ==0){   //usuario incorrecto
+            
+            //alerta usuario incorrecto
             JOptionPane.showMessageDialog(loginView, "Nombre de Usuario o Contraseña incorrecto o el usuario no existe");
-        }else{
-           BVV.setVisible(true);
-           BVV.nombreLBL.setText(modeloUsuario.setNombreUsuario(nombre));
-           BVV.paisLBL.setText(modeloUsuario.setPaisUsuario(nombre));
-           BVV.idUsuarioLBL.setText(nombre);
-           time = modeloUsuario.getSysTime();
-           modeloUsuario.start();
-           loginView.setVisible(false);
-           CerrarController cc = new CerrarController(BVV, modeloUsuario);
-           VueloController VC = new VueloController(BV, VDA);
+        
+        }else{  //usuario correcto
+            
+           BVV.setVisible(true);    //mostrar el menu
+           BVV.nombreLBL.setText(modeloUsuario.setNombreUsuario(nombre));   //obtener nombre
+           BVV.paisLBL.setText(modeloUsuario.setPaisUsuario(nombre));   //obtener pais
+           BVV.idUsuarioLBL.setText(nombre);    //obtener id usuario
+           
+           time = modeloUsuario.getSysTime(); //obtener tiempo dle sistema
+           
+           modeloUsuario.start();   //
+           
+           loginView.setVisible(false); //ocultar vista login
+           
+           CerrarController cc = new CerrarController(BVV, modeloUsuario); //controlador cerrar
+           VueloController VC = new VueloController(BV, VDA);   //controlador vuelo
+           
            VDA.LlenarPais(BV.OrigenCB);
            VDA.LlenarPais(BV.DestinoCB);
            BV.FechaSalida.setMinSelectableDate(modeloUsuario.getSysTime());
